b2b3fa5593a2878ab93329a41dd10a2049d7ccd1
Meghan Pund <meghan.pund@gmail.com>
2021-11-09 10:47:39 -0500

    getting the bot and running

--- app.py
+++ app.py
@@ -1,31 +1,67 @@
 from time import sleep
 from selenium import webdriver
 import secure_info
-
+import random
+from datetime import datetime
+from selenium.webdriver.common.keys import Keys
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 browser = webdriver.Chrome(PATH)
-browser.implicitly_wait(5)
 
-browser.get("http://www.instagram.com")
-print(browser.title)
+def login():
+  
+    browser.implicitly_wait(5)
 
-sleep(2)
+    browser.get("http://www.instagram.com")
+    print(browser.title)
 
-username_input = browser.find_element_by_css_selector("input[name='username']")
-password_input = browser.find_element_by_css_selector("input[name='password']")
+    sleep(2)
 
-username_input.send_keys(secure_info.username)
-password_input.send_keys(secure_info.password)
+    username_input = browser.find_element_by_css_selector("input[name='username']")
+    password_input = browser.find_element_by_css_selector("input[name='password']")
 
-login_button = browser.find_element_by_xpath("//div[text()='Log In']")
-login_button.click()
+    username_input.send_keys(secure_info.username)
+    password_input.send_keys(secure_info.password)
+
+    login_button = browser.find_element_by_xpath("//div[text()='Log In']")
+    login_button.click()
+
+    sleep(3)
 
 
-not_now_button = browser.find_element_by_xpath("//button[text()='Not Now']")
-not_now_button.click()
+def search(tag):
+    address = 'https://www.instagram.com/explore/tags/'
+    browser.get(address + tag)
+    sleep(random.randint(3, 4))
+    browser.find_elements_by_class_name('_9AhH0')[10].click()
+    sleep(random.randint(3, 4))
 
-not_now_button.click()
+def like():
+    global like_cnt
+    try:
+        browser.find_element_by_class_name('coreSpriteRightPaginationArrow').click()
+        sleep(random.randint(3, 4))
+        browser.find_element_by_class_name('fr66n').click()
+        sleep(random.randint(1, 2))
+        like_cnt += 1
+    except:
+        print('Error Occurred While Liking')
+        sleep(5)
+        return 
 
-sleep(5)
+if __name__ == '__main__':
 
+    tags = ['jazz','bebop','hardbop','swing']
+
+    like_cnt = 0
+    begin = datetime.datetime.today()
+    tag = random.choice(tags)
+
+    login()
+    search(tag)
+
+    while like_cnt < 100:
+        like()
+
+    browser.quit()
+    end = datetime.datetime.today()
