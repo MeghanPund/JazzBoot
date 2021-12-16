cbebbad941ff94fe291120ba40b75dd4340a93a9
Meghan Pund <meghan.pund@gmail.com>
2021-12-16 14:34:37 -0500

    commented useful tips throughout program

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from os import close
 from time import sleep
 from selenium import webdriver
-import selenium
 import secure_info
 import random
 from datetime import datetime
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
+# direct the program to the correct webdriver location
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 browser = webdriver.Chrome(PATH)
 
@@ -17,18 +16,20 @@ def login():
     browser.get("http://www.instagram.com")
     print(browser.title)
 
-    sleep(random.randint(1, 3))
+    # we sleep randomly to emulate human interaction and evade bot detection 
+    sleep(random.randint(2, 4))
 
     username_input = browser.find_element_by_css_selector("input[name='username']")
     password_input = browser.find_element_by_css_selector("input[name='password']")
 
+    # input your name and password as string surrounded by quotes
     username_input.send_keys(secure_info.username2)
     password_input.send_keys(secure_info.password2)
 
     login_button = browser.find_element_by_xpath("//div[text()='Log In']")
     login_button.click()
 
-    sleep(random.randint(2, 4))
+    sleep(random.randint(4, 5))
 
 def search(tag):
     address = 'https://www.instagram.com/explore/tags/'
@@ -49,11 +50,13 @@ def like():
 
 def comment():
     
+    # an array of silly comments from which we randomly choose one
     comments = ["Can't stop, won't stop!", "Yaaaaass!!", "Get in there!!", "It always seems impossible until it’s done!", "Enjoy every moment of the journey!", 
     "Get it!", "You’re on fire!", "That’s the way to do it!", "You're absolutely killing it!",]
     silly_comment = random.choice(comments)
     comment.silly_comment = silly_comment
 
+    # program attempts to post comment. If commenting is disabled on post, program alerts user with message.
     try:
         text_box = browser.find_element_by_xpath('/html/body/div[6]/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/textarea')
         text_box.click()
@@ -67,7 +70,8 @@ def comment():
         print('User has disabled comments on this post.')  
       
 
-# update line_prepender, and also update caption to deal with non utf-8 encodable text
+# this funciton logs all of the bot's commenting on the posts by recording the username of the account and the comment that was posted by the bot
+# need to update to prepend to log instead of append, and also update caption to deal with non utf-8 encodable text
 def writeLogToFile():
 
     username = browser.find_element_by_xpath('/html/body/div[6]/div[2]/div/article/div/div[2]/div/div/div[1]/div/header/div[2]/div[1]/div[1]/span/a').text  
@@ -79,7 +83,8 @@ def follow():
     username = browser.find_element_by_xpath('/html/body/div[6]/div[2]/div/article/div/div[2]/div/div/div[1]/div/header/div[2]/div[1]/div[1]/span/a').text
     caption = browser.find_element_by_xpath('/html/body/div[6]/div[2]/div/article/div/div[2]/div/div/div[2]/div[1]/ul/div/li/div/div/div[2]/span').text
     follow_button = browser.find_element_by_xpath('/html/body/div[6]/div[2]/div/article/div/div[2]/div/div/div[1]/div/header/div[2]/div[1]/div[2]/button')
-    # fix if else statement
+    
+    # need to fix if else statement in follow criteria
     if follow_button.text == "Follow":
         try:
             if "jazz" in username or "#jazz" or "music" in caption:
@@ -92,18 +97,22 @@ def follow():
     elif follow_button.text == "Following":
         return(print("already following " + username))
            
-
+# we pass in the number of posts with which we want to interact as an integer and this function searches for a hashtag, then executes like, comment, and follow on recent 
+# posts while keeping track of each interaction by logging it in IG_log.txt
 def run_bot(num_of_interactions=int):
 
+    # like random comments, but jazz-adjacent hashtags from which the program will randomly choose and search
     tags = ['jazz','bebop','hardbop', 'transcribe', 'bigband', 'jazzband', 'saxophone', 'jazzsax', 'charlieparker', 'dizzygillespie',
     'jazztrumpet', 'jazzdrums', 'jazztrombone', 'jazzorgan', 'jazzpiano', 'jazzbass', 'cooljazz', 'jazzmusic', 'transcription',]
     tag = random.choice(tags)
     
+    # initiate like_count at 0 so we keep track of the number of posts interacted with
     like_count = 0
     
     login()
     search(tag)
     
+    # contiue liking and commenting on posts and then following the user until the prescribed number (passed into run_bot function) is reached
     while like_count < num_of_interactions:
         like()
         comment()
